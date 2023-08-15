0e27e7b69815044c3c3b326442d4450c587bbad0
90oak <32446681+90oak@users.noreply.github.com>
2023-08-14 20:12:12 -0700

    Update __init__.py
    Move data cleanup above isnumeric

--- cgmquantify/__init__.py
+++ cgmquantify/__init__.py
@@ -51,16 +51,18 @@ def importdexcom(filename):
   data = pd.read_csv(filename) 
   df = pd.DataFrame()
   df['Time'] = data['Timestamp (YYYY-MM-DDThh:mm:ss)']
-  df['Glucose'] = pd.to_numeric(data['Glucose Value (mg/dL)'])
-  df.drop(df.index[:12], inplace=True)
+
+  #rename Glucose table
+  df['Glucose'] = data['Glucose Value (mg/dL)'])
 
   #drop empty rows. These are created for food logging events, etc.
   df = df.dropna(subset=['Glucose'])
 
   #need to change LOW to 40 to clean up the data in the glucose column
   df['Glucose'] = df['Glucose'].replace('LOW', '40')
-  df['Glucose'] = pd.to_numeric(df['Glucose'])
-  
+
+  df['Glucose'] = pd.to_numeric(data['Glucose'])
+  df.drop(df.index[:12], inplace=True) 
   df['Time'] =  pd.to_datetime(df['Time'], format='%Y-%m-%dT%H:%M:%S')
   df['Day'] = df['Time'].dt.date
   df = df.reset_index()
